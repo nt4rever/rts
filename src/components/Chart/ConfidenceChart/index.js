9080ac29bde2321a9280e7b7ef68c57ee96ffb3a
dangquangtai <quangtai26801@gmail.com>
2023-12-20 21:41:27 +0700

    add label classes

--- src/components/Chart/ConfidenceChart/index.js
+++ src/components/Chart/ConfidenceChart/index.js
@@ -1,21 +1,25 @@
+import { useTranslation } from "next-i18next";
 import styles from "./index.module.scss";
 
 const ConfidenceChart = ({ data }) => {
   return (
     <div className={styles.wrapper}>
-      {data.sort((a,b) => b.score - a.score).map((item) => (
-        <ConfidenceChartRow key={item.label} {...item} />
-      ))}
+      {data
+        .sort((a, b) => b.score - a.score)
+        .map((item) => (
+          <ConfidenceChartRow key={item.label} {...item} />
+        ))}
     </div>
   );
 };
 
 const ConfidenceChartRow = ({ label, score }) => {
+  const { t } = useTranslation();
   return (
     <div className={styles.row}>
       <div className={styles.illustration}>
         <div className={styles.bar} style={{ width: `${score * 100}%` }}></div>
-        <span>{label}</span>
+        <span>{t(`constraint.ai-level.${label}`)}</span>
       </div>
       <span>{Number(score).toFixed(4)}</span>
     </div>
