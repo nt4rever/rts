b8d6572b9c3cc116eb7dbc9b7da7b39dcce71064
dangquangtai <quangtai26801@gmail.com>
2023-12-24 11:13:17 +0700

    fix color blur

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -1,14 +1,34 @@
 import LanguageSwitcher from "@/components/language-switcher";
+import { ActionIcon } from "@mantine/core";
 import { Divider } from "@mui/material";
+import { CornerRightUp } from "react-feather";
 import styles from "./index.module.scss";
 
 export default function Footer() {
+  const backToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   return (
     <div className={styles.footer}>
       <Divider />
       <div className={styles.reserveBox}>
         <div>@2023 RTS. All rights reserved</div>
-        <LanguageSwitcher />
+        <div className={styles.groupBtn}>
+          <LanguageSwitcher />
+          <ActionIcon
+            title="Back to top"
+            variant="gradient"
+            size={37}
+            aria-label="Gradient action icon"
+            gradient={{ from: "#6366F1", to: "violet", deg: 32 }}
+            onClick={backToTop}
+            style={{
+              borderRadius: 8,
+            }}
+          >
+            <CornerRightUp size={18} />
+          </ActionIcon>
+        </div>
       </div>
     </div>
   );
