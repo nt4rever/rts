d686b72b6cc91f43fc7856b981652a0727ce5901
dangquangtai <quangtai26801@gmail.com>
2024-01-08 19:43:20 +0700

    deploy static page

---
+++ src/pages/p/[slug].js
@@ -0,0 +1,85 @@
+import { pageService } from "@/apis/page";
+import { TransitionPage } from "@/components/transition";
+import MainLayout from "@/layouts/main/layout";
+import HomeSponsor from "@/sections/home/sponsor";
+import { Box, Container, Stack } from "@mui/material";
+import { QueryClient } from "@tanstack/react-query";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
+import { NotFoundComponent } from "../404";
+
+const Page = ({ isError, data }) => {
+  if (isError) {
+    return <NotFoundComponent />;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{data?.title || "Page"} | RTS</title>
+        <meta name="description" content={data?.brief_content} />
+        <meta property="og:title" content={data?.title} />
+        <meta property="og:description" content={data?.description_SEO} />
+      </Head>
+      <TransitionPage>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            py: 2,
+          }}
+        >
+          <Container maxWidth="lg">
+            <div
+              className="static-page-content"
+              dangerouslySetInnerHTML={{
+                __html: data?.content,
+              }}
+            ></div>
+            <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <HomeSponsor />
+            </Stack>
+          </Container>
+        </Box>
+      </TransitionPage>
+    </>
+  );
+};
+
+export const getServerSideProps = async (ctx) => {
+  const queryClient = new QueryClient();
+  const { slug } = ctx.params;
+  const params = {
+    type: "OTHER",
+    slug,
+  };
+  let isError = false;
+  let data = null;
+  const locale = ctx.locale || "vi";
+
+  try {
+    data = await queryClient.fetchQuery(["static-pages", params], () =>
+      pageService.get(params)
+    );
+    const content = data?.content?.find((ele) => ele?.lang === locale)?.value;
+    data.content = content;
+  } catch (error) {
+    isError = true;
+  }
+
+  return {
+    props: {
+      isError,
+      data,
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+};
+
+Page.getLayout = (page) => <MainLayout>{page}</MainLayout>;
+
+export default Page;
